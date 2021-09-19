f84e589285cce2cb52bae346107c34aaf5ce9dca
Matthew Taylor <MatthewAlanTaylor@outlook.com>
2021-09-19 04:10:59 -0400

    Update styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import MapImage from "./resources/green-map.png";
 
 function App() {
+  const [data, setData] = React.useState(null);
+  const [mode, setMode] = React.useState(0);
+
+  /*
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+  */
+
+  React.useEffect(() => {
+    fetch("https://api.weather.gov/alerts/active/area/FL", {
+      method: "GET",
+      headers: new Headers({
+        "User-Agent": "(advd, matthewalantaylor2@gmail.com)"
+      })
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data["type"]))
+      .catch((error) => console.log(error));
+  }, []);
+
+  function switchMode(e) {
+    if (mode < 2) {
+        setMode(mode + 1);
+    }
+    else {
+      setMode(0);
+    }
+    console.log(mode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div id="button" onClick={switchMode}><p>Mode</p></div>
+      <table id="indicator-table">
+        <tbody>
+          <tr>
+            <td><div className={"indicator-light" + (mode === 0 ? " on" : "")}></div></td>
+            <td><div className={"indicator-light" + (mode === 1 ? " on" : "")}></div></td>
+            <td><div className={"indicator-light" + (mode === 2 ? " on" : "")}></div></td>
+          </tr>
+          <tr>
+            <td><p>On</p></td>
+            <td><p>Auto</p></td>
+            <td><p>Off</p></td>
+          </tr>
+        </tbody>
+      </table>
+      <div id="bg-window"></div>
+      <div className="centered">
+        <div id="img-wrapper">
+          <div id="loc-point"></div>
+          <img id="map-img" src={MapImage} alt="map" />
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 

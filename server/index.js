765afa55500665b98919acb5a6e04ea347b8f05d
Matthew Taylor <matthewalantaylor@outlook.com>
2021-09-19 07:05:05 -0700

    Fix frontend integration

--- server/index.js
+++ server/index.js
@@ -4,19 +4,28 @@ const fs = require("fs");
 const port = process.env.PORT || 3001;
 const app = express();
 
-let fifoWriteStream = fs.createWriteStream("/tmp/pipe_a");
+cp.spawn('rm', ['-f', '/tmp/pipe_a']);
+let fifop = cp.spawn('mkfifo', ["/tmp/pipe_a"]);  // Create Pipe B
+fifop.on('exit', function(status) {
+	if(status != 0) console.log('oh god oh fuck');
+});
+
 
 let lastChild = null;
 
 app.get("/api", (req, res) => {
 	if (req.query.command == "on") {
 		console.log("open");
-		cp.exec(".//home/jaytlang/Documents/advd/advd");
+		cp.exec("/home/theninja583/Documents/advd/advd");
 		res.send({status: "open success"});
 	}
 	else if (req.query.command == "off") {
 		console.log("close");
-		fifoWriteStream.write("1");
+		let yeet = cp.spawn("./write.sh");
+		yeet.on('exit', function(status) {
+			if(status != 0) console.log('sad');
+		});
+		res.send({status: "close success"});
 	}
 	else {
 		res.send({status: "no action taken"});

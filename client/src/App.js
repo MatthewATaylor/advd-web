583fe79f42fb49705f521394da6b3a4bdc75c81d
Matthew Taylor <MatthewAlanTaylor@outlook.com>
2021-09-19 07:42:06 -0400

    Add networking process control

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,22 @@ import React from "react";
 import './App.css';
 import MapImage from "./resources/green-map.png";
 
+let latitude = null;
+let longitude = null;
+
+function showPosition(position) {
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+}
+
+
+
 function App() {
-  const [data, setData] = React.useState(null);
   const [mode, setMode] = React.useState(0);
+  const [locX, setLocX] = React.useState(0);
+  const [locY, setLocY] = React.useState(0);
 
   /*
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-  */
-
   React.useEffect(() => {
     fetch("https://api.weather.gov/alerts/active/area/FL", {
       method: "GET",
@@ -22,23 +26,59 @@ function App() {
       })
     })
       .then((res) => res.json())
-      .then((data) => console.log(data["type"]))
+      .then((data) => console.log(data.type))
       .catch((error) => console.log(error));
   }, []);
+  */
 
-  function switchMode(e) {
-    if (mode < 2) {
-        setMode(mode + 1);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+
+  function setLocation(e) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    }
+
+    if (latitude !== null && longitude !== null) {
+      console.log(longitude);
+      let xPos = (-longitude - 65) / 65.0;
+      console.log(xPos * 100);
+      setLocX(xPos * 100);
+
+      console.log(latitude);
+      let yPos = (latitude - 25) / 25.0;
+      setLocY(yPos * 100);
+      console.log(yPos);
     }
     else {
-      setMode(0);
+      console.log("Failed to access geolocation");
     }
-    console.log(mode);
+  }
+
+  function switchMode(e) {
+    if (mode === 0) {
+      setMode(1);
+    }
+    else if (mode === 1) {
+      setMode(2);
+      fetch("/api?command=on")
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.log(error));
+    }
+    else if (mode === 2) {
+      setMode(0);
+      fetch("/api?command=off")
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.log(error));
+    }
   }
 
   return (
     <React.Fragment>
-      <div id="button" onClick={switchMode}><p>Mode</p></div>
+      <div className="button" id="mode-button" onClick={switchMode}><p>Mode</p></div>
       <table id="indicator-table">
         <tbody>
           <tr>
@@ -47,19 +87,22 @@ function App() {
             <td><div className={"indicator-light" + (mode === 2 ? " on" : "")}></div></td>
           </tr>
           <tr>
-            <td><p>On</p></td>
-            <td><p>Auto</p></td>
             <td><p>Off</p></td>
+            <td><p>Auto</p></td>
+            <td><p>On</p></td>
           </tr>
         </tbody>
       </table>
-      <div id="bg-window"></div>
+      <div id="bg-window">
+        <h1 id="title">&lt;advd/&gt;</h1>
+      </div>
       <div className="centered">
         <div id="img-wrapper">
-          <div id="loc-point"></div>
+          <div id="loc-point" style={{right: locX.toString() + "%", bottom: locY.toString() + "%"}}></div>
           <img id="map-img" src={MapImage} alt="map" />
         </div>
       </div>
+      <div className="button" id="loc-button" onClick={setLocation}><p>Set Location</p></div>
     </React.Fragment>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const cp = require("child_process");
 const port = process.env.PORT || 3001;
 const app = express();
 
+let lastChild = null;
+
 app.get("/api", (req, res) => {
-	res.json({ message: `Hello! req.query.x: ${req.query.x}` });
+	if (req.query.command == "on") {
+		console.log("open");
+		if (lastChild !== null) {
+			lastChild.kill();
+		}
+		//lastChild = cp.fork("processPath");
+		res.send({status: "open success"});
+	}
+	else if (req.query.command == "off") {
+		console.log("close");
+		if (lastChild !== null) {
+			lastChild.kill();
+			res.send({status: "close success"});
+		}
+		else {
+			res.send({status: "nothing to close"});
+		}
+	}
+	else {
+		res.send({status: "no action taken"});
+	}
 });
 
 app.listen(port, () => {
